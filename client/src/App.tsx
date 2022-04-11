a7f4ea19d9e3a2f02f2aba9d158f5e73ac67ca22
Lope Ariyo <lopeariyo@gmail.com>
2022-04-11 19:39:59 +0100

    feat: implement basic ui
    - Add number inputs on the client side for placeholder to gather savings, deposit and interest rates
    - start skeleton for README

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, Flex, Heading, Text, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper} from '@chakra-ui/react'
 import { Container } from '@chakra-ui/react'
 import DefaultLayout from './components/layouts/Default'
 import LineChart from './components/LineChart'
@@ -18,18 +18,77 @@ const tempData = {
 function App() {
     return (
         <ChakraProvider theme={defaultTheme}>
-            {/* We've just bundled everything into one file here to 
-            get you started!*/}
             <DefaultLayout>
+                <Heading as="h1" paddingBottom="20px" color="text">
+                        Interest Rate Calculator
+                </Heading>
                 <Container pt={6}>
                     <LineChart
-                        title="Savings Over time"
+                        title="Savings Over Time"
                         xAxisData={tempData.xAxis}
                         yAxisData={tempData.yAxis}
                         xLabel="Years"
                         yLabel="Amount"
                     />
                 </Container>
+                <Flex>
+                    <Flex>
+                        <Text>Savings Amount</Text>
+                        <NumberInput 
+                            defaultValue={0} 
+                            min={0}
+                            step={1000} 
+                            label="Savings Amount"
+                            name="savingsAmount"
+                            size='sm' 
+                            maxW={20}
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </Flex>
+                    <Flex>
+                        <Text>Monthly Deposit</Text>
+                        <NumberInput 
+                            defaultValue={0} 
+                            min={0}
+                            step={100} 
+                            label="Monthly Deposit"
+                            name="monthlyDeposit"
+                            size='sm' 
+                            maxW={20}
+                        >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput>
+                    </Flex>
+                    <Flex>
+                        <Text>Interest Rate</Text>
+                        <NumberInput
+                                label="Interest Rate"
+                                name="interestRare"
+                                placeholder="100"
+                                defaultValue={0} 
+                                precision={2} 
+                                min={0}
+                                step={0.1}
+                                size='sm' 
+                                maxW={20}
+                            >
+                            <NumberInputField />
+                            <NumberInputStepper>
+                            <NumberIncrementStepper />
+                            <NumberDecrementStepper />
+                            </NumberInputStepper>
+                        </NumberInput> 
+                    </Flex>
+                </Flex>
             </DefaultLayout>
         </ChakraProvider>
     )
